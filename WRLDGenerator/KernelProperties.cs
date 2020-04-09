36d9dd5c5ac656d0a0d617d4a80cbf14d74fddfa
Aetherpoint <inharmony88@gmail.com>
2020-04-09 00:44:51 -0400

    Adds class and simplejson

---
+++ WRLDGenerator/KernelProperties.cs
@@ -0,0 +1,17 @@
+﻿
+/// <summary>
+/// The class for properties we want to access for each
+/// kernel to constract our models with later
+/// </summary>
+[System.Serializable]
+public class KernelProperties
+{
+
+    // Currently we just support the elevation of the individual structure
+    public float heightLevel;
+    
+    public KernelProperties(float level)
+    {
+        heightLevel = level;
+    }
+}
